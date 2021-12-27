476d20ad21bfa9533ff1289621b3015e09c6fdfd
Ayush Rao <ayushvijayrao@gmail.com>
2021-12-27 11:51:58 +0530

    Create _1_AddStrings.java

---
+++ src/Strings/_1_AddStrings.java
@@ -0,0 +1,20 @@
+package Strings;
+
+public class _1_AddStrings {
+    public String addStrings(String num1, String num2) {
+        int index1 = num1.length() - 1;
+        int index2 = num2.length() - 1;
+        int carry = 0, temp1, temp2, temp3, counter = 0;
+        StringBuilder sum = new StringBuilder();
+        while (index1 >= 0 || index2 >= 0 || carry != 0) {
+            temp1 = index1 < 0 ? 0 : Character.getNumericValue(num1.charAt(index1--));
+            temp2 = index2 < 0 ? 0 : Character.getNumericValue(num2.charAt(index2--));
+            temp3 = temp1 + temp2 + carry;
+            carry = temp3 / 10 != 0 ? 1 : 0;
+            temp3 = temp3 % 10;
+            sum.insert(0, String.valueOf(temp3));
+            counter++;
+        }
+        return (sum.toString());
+    }
+}
