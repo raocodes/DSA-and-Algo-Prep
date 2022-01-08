072c4311dfe0a541fd24b43ee9e630d46797fa5a
Ayush Rao <ayushvijayrao@gmail.com>
2022-01-09 01:50:26 +0300

    Create _9_RemoveDuplicates.java

---
+++ Medium/LinkedList/_9_RemoveDuplicates/_9_RemoveDuplicates.java
@@ -0,0 +1,44 @@
+package Medium.LinkedList._9_RemoveDuplicates;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class _9_RemoveDuplicates {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode prev = null, temp = head;
+        while (temp != null) {
+            if (temp.next != null && temp.next.val == temp.val) {
+                while (temp.next != null && temp.next.val == temp.val) {
+                    temp = temp.next;
+                }
+                temp = temp.next;
+                if (prev != null) {
+                    prev.next = temp;
+                } else {
+                    head = temp;
+                }
+            } else {
+                prev = temp;
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+}
