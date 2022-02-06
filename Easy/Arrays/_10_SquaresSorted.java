3b7f7dcf7e611bc154c7694565f671e1400050f3
Ayush Rao <ayushvijayrao@gmail.com>
2022-02-06 12:21:48 +0300

    Create _10_SquaresSorted.java

---
+++ Easy/Arrays/_10_SquaresSorted.java
@@ -0,0 +1,14 @@
+package Easy.Arrays;
+
+import java.util.Arrays;
+
+public class _10_SquaresSorted {
+    public int[] sortedSquares(int[] nums) {
+        int[] squares = new int[nums.length];
+        for (int a = 0; a < nums.length; a++) {
+            squares[a] = (int) Math.pow(nums[a], 2);
+        }
+        Arrays.sort(squares);
+        return squares;
+    }
+}
