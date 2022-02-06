e822cf1c2e1ababb1321a473be0587bb1244f939
Ayush Rao <ayushvijayrao@gmail.com>
2022-02-06 11:16:19 +0300

    Create _7_PivotIndex.java

---
+++ Easy/Arrays/_7_PivotIndex.java
@@ -0,0 +1,18 @@
+package Easy.Arrays;
+
+public class _7_PivotIndex {
+    public int pivotIndex(int[] nums) {
+        int sum = 0, left = 0;
+        for (int elem : nums) {
+            sum += elem;
+        }
+        for (int a = 0; a < nums.length; a++) {
+            if (a != 0) {
+                left += nums[a - 1];
+            }
+            if ((sum - left - nums[a]) == left)
+                return a;
+        }
+        return -1;
+    }
+}
