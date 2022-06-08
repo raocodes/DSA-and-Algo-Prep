2386a587a3c6738ce40db238c5b19eb0d86ff921
Ayush Rao <ayushvijayrao@gmail.com>
2022-06-08 10:43:41 +0300

    Update _1_BinarySearch.java

--- src/BinarySearch_AV/_1_BinarySearch.java
+++ src/BinarySearch_AV/_1_BinarySearch.java
@@ -7,7 +7,7 @@ public class _1_BinarySearch {
         int end = n - 1;
 
         while (start <= end) {
-            int mid = (start + end) / 2;
+            int mid = start + ((end - start) / 2); // To prevent overflow
             if (arr[mid] == k) return mid;
             else if (k < arr[mid]) end = mid - 1;
             else start = mid + 1;
