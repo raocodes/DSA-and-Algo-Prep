33482afc38067a5e06cf8ba61fca12bdc9531b45
Ayush Rao <ayushvijayrao@gmail.com>
2022-06-08 22:39:31 +0300

    Update _5_NumberOfRotations.java

--- src/BinarySearch_AV/_5_NumberOfRotations.java
+++ src/BinarySearch_AV/_5_NumberOfRotations.java
@@ -1,6 +1,6 @@
 package BinarySearch_AV;
 
-// https://www.youtube.com/watch?v=4WmTRFZilj8&list=PL_z_8CaSLPWeYfhtuKHj-9MpYb6XQJ_f2&index=8
+// https://practice.geeksforgeeks.org/problems/rotation4723/1/
 public class _5_NumberOfRotations {
     int findKRotation(int arr[], int n) {
         int low = 0;
