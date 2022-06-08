0b8aa87392a63f62d23b175c45dc53624011d1bf
Ayush Rao <ayushvijayrao@gmail.com>
2022-06-08 23:04:12 +0300

    Update _6_FindElementInRotatedArray.java

--- src/BinarySearch_AV/_6_FindElementInRotatedArray.java
+++ src/BinarySearch_AV/_6_FindElementInRotatedArray.java
@@ -1,6 +1,8 @@
 package BinarySearch_AV;
 
 // https://leetcode.com/problems/search-in-rotated-sorted-array/submissions/
+// https://leetcode.com/problems/search-in-rotated-sorted-array-ii/submissions/
+
 public class _6_FindElementInRotatedArray {
     public int search(int[] nums, int target) {
         int start = 0;
@@ -30,5 +32,27 @@ public class _6_FindElementInRotatedArray {
             else start = mid + 1;
         }
         return -1;
+    }
+
+    public boolean searchii(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + ((end - start) / 2);
+            if (nums[mid] == target) return true;
+            if (nums[mid] > nums[start]) {
+                // The left half is sorted
+                if (target < nums[mid] && target >= nums[start]) end = mid - 1;
+                else start = mid + 1;
+            } else if (nums[mid] < nums[start]) {
+                // The right half is sorted
+                if (target > nums[mid] && target <= nums[end]) start = mid + 1;
+                else end = mid - 1;
+            } else {
+                // Cannot figure out if the array is sorted so we just move start
+                // here start == mid and we already checked if it is target so we ignore value
+                start++;
+            }
+        }
+        return false;
     }
 }
