f7c282b8d1def8aaff46e361a26c0bdf3ab5bf65
Ayush Rao <ayushvijayrao@gmail.com>
2022-06-08 10:37:31 +0300

    Create _1_BinarySearch.java

---
+++ src/BinarySearch_AV/_1_BinarySearch.java
@@ -0,0 +1,17 @@
+package BinarySearch_AV;
+
+// https://practice.geeksforgeeks.org/problems/binary-search/1
+public class _1_BinarySearch {
+    int binarysearch(int arr[], int n, int k) {
+        int start = 0;
+        int end = n - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == k) return mid;
+            else if (k < arr[mid]) end = mid - 1;
+            else start = mid + 1;
+        }
+        return -1;
+    }
+}
