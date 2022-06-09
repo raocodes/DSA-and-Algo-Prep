41057be2c515e4e4b6fc7305d5c921111c2684e7
Ayush Rao <ayushvijayrao@gmail.com>
2022-06-09 08:41:27 +0300

    Create _1_ReverseLinkedList.java

---
+++ src/StriverDSASheet/LinkedList/_1_ReverseLinkedList.java
@@ -0,0 +1,22 @@
+package StriverDSASheet.LinkedList;
+
+public class _1_ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode temp = head;
+        ListNode prev = null;
+        ListNode next = null;
+
+        while (temp != null) {
+            next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
+        }
+
+        return prev;
+    }
+}
