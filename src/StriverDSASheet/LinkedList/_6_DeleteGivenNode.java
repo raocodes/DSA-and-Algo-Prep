6c77a8e9a1bd7abe61846e72638a9d34ba8c0a82
Ayush Rao <ayushvijayrao@gmail.com>
2022-06-09 10:34:26 +0300

    Create _6_DeleteGivenNode.java

---
+++ src/StriverDSASheet/LinkedList/_6_DeleteGivenNode.java
@@ -0,0 +1,13 @@
+package StriverDSASheet.LinkedList;
+
+public class _6_DeleteGivenNode {
+    public void deleteNode(ListNode node) {
+        ListNode prev = null;
+        while (node.next != null) {
+            node.val = node.next.val;
+            prev = node;
+            node = node.next;
+        }
+        prev.next = null;
+    }
+}
