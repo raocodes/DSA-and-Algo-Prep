3eee2bff779f0fc4f141bf38ba5345467d3f738d
Ayush Rao <ayushvijayrao@gmail.com>
2022-06-12 13:03:13 +0300

    Update _1_AddTwoNums.java

--- others/leetcode_sheet/medium/linkedlist/_1_AddTwoNums.java
+++ others/leetcode_sheet/medium/linkedlist/_1_AddTwoNums.java
@@ -1,22 +1,5 @@
 package others.leetcode_sheet.medium.linkedlist;
 
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
-
 public class _1_AddTwoNums {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode temp = null, head = null;
