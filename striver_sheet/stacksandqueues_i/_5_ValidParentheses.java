77321f962f289c6b877d32d78b4ce90d221c28a3
Ayush Rao <ayushvijayrao@gmail.com>
2022-06-13 15:46:02 +0300

    Create _5_ValidParentheses.java

---
+++ striver_sheet/stacksandqueues_i/_5_ValidParentheses.java
@@ -0,0 +1,32 @@
+package striver_sheet.stacksandqueues_i;
+
+import java.util.Stack;
+import java.util.Map;
+import java.util.HashMap;
+
+public class _5_ValidParentheses {
+    public boolean isValid(String s) {
+        // types are (, { and [
+        Stack<Character> stk = new Stack<>();
+
+        Map<Character, Character> balance = new HashMap<>();
+        balance.put(')', '(');
+        balance.put('}', '{');
+        balance.put(']', '[');
+
+        for (int i = 0; i < s.length(); i++) {
+            char curr = s.charAt(i);
+            if (curr == '(' || curr == '{' || curr == '[') {
+                stk.push(curr);
+            } else if (curr == ')' || curr == '}' || curr == ']') {
+                if (!stk.isEmpty() && stk.peek() == balance.get(curr)) {
+                    stk.pop();
+                } else {
+                    stk.push(curr);
+                }
+            }
+        }
+
+        return stk.isEmpty();
+    }
+}
