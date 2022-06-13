cca7df1ba488269111882d25f432bc8c287f9869
Ayush Rao <ayushvijayrao@gmail.com>
2022-06-13 23:09:23 +0300

    Create _6_NextGreaterElement.java

---
+++ striver_sheet/stacksandqueues_i/_6_NextGreaterElement.java
@@ -0,0 +1,31 @@
+package striver_sheet.stacksandqueues_i;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class _6_NextGreaterElement {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] result = new int[nums1.length];
+        Map<Integer, Integer> ngr = new HashMap<>();
+        Stack<Integer> stk = new Stack<>();
+
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!stk.isEmpty() && stk.peek() <= nums2[i]) {
+                stk.pop();
+            }
+            if (stk.isEmpty()) {
+                ngr.put(nums2[i], -1);
+                stk.push(nums2[i]);
+            } else {
+                ngr.put(nums2[i], stk.peek());
+                stk.push(nums2[i]);
+            }
+        }
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = ngr.get(nums1[i]);
+        }
+
+        return result;
+    }
+}
