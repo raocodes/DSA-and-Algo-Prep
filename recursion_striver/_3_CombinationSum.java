94f7f9f7e9847b67012ec372a63f7ee846f4f292
Ayush Rao <ayushvijayrao@gmail.com>
2022-06-18 23:14:01 +0300

    Update _3_CombinationSum.java

--- recursion_striver/_3_CombinationSum.java
+++ recursion_striver/_3_CombinationSum.java
@@ -3,12 +3,13 @@ package recursion_striver;
 import java.util.ArrayList;
 import java.util.List;
 
+// https://leetcode.com/problems/combination-sum/
 public class _3_CombinationSum {
     public void findCombinations(int index, int target, int[] candidates, List<Integer> sequence,
             List<List<Integer>> result) {
         if (target == 0) {
             // Then the sum of the sequence is target
-            result.add(sequence);
+            result.add(new ArrayList<>(sequence));
             return;
         }
 
@@ -21,10 +22,10 @@ public class _3_CombinationSum {
         if (candidates[index] <= target) {
             // After subtracting from target it will not become zero
             sequence.add(candidates[index]);
-            findCombinations(index, target - candidates[index], candidates, new ArrayList<>(sequence), result);
+            findCombinations(index, target - candidates[index], candidates, sequence, result);
             sequence.remove(sequence.size() - 1);
         }
-        findCombinations(index + 1, target, candidates, new ArrayList<>(sequence), result);
+        findCombinations(index + 1, target, candidates, sequence, result);
     }
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
