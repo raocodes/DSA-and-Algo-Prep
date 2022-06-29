6906890787a1bd869cbed0ba4ea82e68f921eb73
Ayush Rao <ayushvijayrao@gmail.com>
2022-06-30 00:43:18 +0530

    Striver recursion

---
+++ striver_sheet/recursion/_6_KthPermutation.java
@@ -0,0 +1,39 @@
+package striver_sheet.recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// https://leetcode.com/problems/permutation-sequence/
+public class _6_KthPermutation {
+    public String getPermutation(int n, int k) {
+        // First we generate factorial to find out the total number of permutations
+        int fact = 1, i;
+        List<Integer> numbers = new ArrayList<>();
+        for (i = 1; i < n; i++) {
+            fact = fact * i;
+            numbers.add(i);
+        }
+        numbers.add(i);
+        // Numbers will contain everything
+        // fact will contain fact of n - 1
+        // Basically means the number of values possible if one element is chosen
+
+        k = k - 1; // We follow zero based indexing
+        String result = new String("");
+        while (true) {
+            // Choosing value
+            result = result.concat(String.valueOf(numbers.get(k / fact)));
+            numbers.remove(k / fact);
+            if (numbers.size() == 0) {
+                // If we run out of numbers means the permutation is found
+                break;
+            }
+
+            // Updating values
+            k = k % fact;
+            fact = fact / numbers.size();
+        }
+
+        return result;
+    }
+}
