b447ce33e5abcffc3e1a330fa4a8f75682a9c98a
Ayush Rao <ayushvijayrao@gmail.com>
2022-07-10 13:25:30 +0530

    Create _4_LevelOrderTraversal.java

---
+++ trees_striver/_4_LevelOrderTraversal.java
@@ -0,0 +1,37 @@
+package trees_striver;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+// https://leetcode.com/problems/binary-tree-level-order-traversal/
+public class _4_LevelOrderTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            List<Integer> toaddinresult = new ArrayList<>();
+            int sizeOfLevel = queue.size();
+            for (int i = 0; i < sizeOfLevel; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                toaddinresult.add(node.val);
+            }
+            result.add(toaddinresult);
+        }
+
+        return result;
+    }
+}
