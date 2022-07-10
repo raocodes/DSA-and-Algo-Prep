0f33663fc6184f024116210db67e039ce7f29a51
Ayush Rao <ayushvijayrao@gmail.com>
2022-07-10 14:17:52 +0530

    Create _6_IterativeInorderTraversal.java

---
+++ trees_striver/_6_IterativeInorderTraversal.java
@@ -0,0 +1,36 @@
+package trees_striver;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+// https://leetcode.com/problems/binary-tree-inorder-traversal/
+public class _6_IterativeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stk = new Stack<>();
+        TreeNode currnode = root;
+        while (true) {
+            if (currnode != null) {
+                // Go left and add node to stack
+                stk.add(currnode);
+                currnode = currnode.left;
+            } else {
+                if (stk.isEmpty()) {
+                    // Stack is empty so there is nothing to pop and current is null
+                    break;
+                } else {
+                    currnode = stk.pop();
+                    result.add(currnode.val);
+                    currnode = currnode.right;
+                }
+            }
+        }
+
+        return result;
+    }
+}
