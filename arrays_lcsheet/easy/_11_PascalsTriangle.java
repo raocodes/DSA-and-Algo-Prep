e916573bacbe02b3daf2bc2cb3c8d8d9772639cc
Ayush Rao <ayushvijayrao@gmail.com>
2022-07-13 10:37:40 +0530

    Create _11_PascalsTriangle.java

---
+++ arrays_lcsheet/easy/_11_PascalsTriangle.java
@@ -0,0 +1,23 @@
+package arrays_lcsheet.easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// https://leetcode.com/problems/pascals-triangle/
+public class _11_PascalsTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<Integer>(Arrays.asList(new Integer[] { 1 })));
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            row.add(1);
+            for (int j = 1; j < i; j++) {
+                row.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+            }
+            row.add(1);
+            result.add(row);
+        }
+        return result;
+    }
+}
