1f1415450a917f259d4ecd9f308d762eee692104
Ayush Rao <ayushvijayrao@gmail.com>
2022-07-15 00:50:55 +0530

    Create TreeNode.java

---
+++ trees_striver/_16_BoundaryTraversal/TreeNode.java
@@ -0,0 +1,14 @@
+package trees_striver._16_BoundaryTraversal;
+
+public class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+}
