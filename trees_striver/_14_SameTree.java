b525f627359da54897ff4b2b14123bf457ff3cfc
Ayush Rao <ayushvijayrao@gmail.com>
2022-07-15 00:10:02 +0530

    Create _14_SameTree.java

---
+++ trees_striver/_14_SameTree.java
@@ -0,0 +1,25 @@
+package trees_striver;
+
+// https://leetcode.com/problems/same-tree/
+public class _14_SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null) {
+            return p == q;
+            // Sometimes they both can
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        if (!isSameTree(p.left, q.left)) {
+            return false;
+        }
+
+        if (!isSameTree(p.right, q.right)) {
+            return false;
+        }
+
+        return true;
+    }
+}
