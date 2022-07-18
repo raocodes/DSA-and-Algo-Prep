7f47876c85ed166b4e3c8b1aebde5430d5631564
Ayush Rao <ayushvijayrao@gmail.com>
2022-07-18 12:26:54 +0530

    Update _18_TopView.java

--- trees_striver/_18_TopView.java
+++ trees_striver/_18_TopView.java
@@ -9,43 +9,43 @@ import java.util.TreeMap;
 class NodeObj {
     Node node;
     int vertical;
-    
-    NodeObj(Node node, int vertical){
+
+    NodeObj(Node node, int vertical) {
         this.node = node;
         this.vertical = vertical;
     }
 }
 
+// https://practice.geeksforgeeks.org/problems/top-view-of-binary-tree/1
 public class _18_TopView {
-    static ArrayList<Integer> topView(Node root)
-    {
+    static ArrayList<Integer> topView(Node root) {
         ArrayList<Integer> result = new ArrayList<>();
         Queue<NodeObj> queue = new LinkedList<>();
         TreeMap<Integer, Integer> map = new TreeMap<>();
         queue.add(new NodeObj(root, 0));
-        
-        while(!queue.isEmpty()){
+
+        while (!queue.isEmpty()) {
             NodeObj nodeobj = queue.poll();
             int vertical = nodeobj.vertical;
             Node node = nodeobj.node;
-            
-            if(!map.containsKey(vertical)){
+
+            if (!map.containsKey(vertical)) {
                 map.put(vertical, node.data);
             }
-            
-            if(node.left != null){
+
+            if (node.left != null) {
                 queue.add(new NodeObj(node.left, vertical - 1));
             }
-            
-            if(node.right != null){
+
+            if (node.right != null) {
                 queue.add(new NodeObj(node.right, vertical + 1));
             }
         }
-        
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             result.add(entry.getValue());
         }
-        
+
         return result;
     }
 }
