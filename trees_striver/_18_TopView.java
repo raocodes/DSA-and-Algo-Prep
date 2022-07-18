27fcdabd76aaa29e837f39e1433c8679e8db9ae2
Ayush Rao <ayushvijayrao@gmail.com>
2022-07-18 12:25:27 +0530

    Create _18_TopView.java

---
+++ trees_striver/_18_TopView.java
@@ -0,0 +1,51 @@
+package trees_striver;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.TreeMap;
+
+class NodeObj {
+    Node node;
+    int vertical;
+    
+    NodeObj(Node node, int vertical){
+        this.node = node;
+        this.vertical = vertical;
+    }
+}
+
+public class _18_TopView {
+    static ArrayList<Integer> topView(Node root)
+    {
+        ArrayList<Integer> result = new ArrayList<>();
+        Queue<NodeObj> queue = new LinkedList<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        queue.add(new NodeObj(root, 0));
+        
+        while(!queue.isEmpty()){
+            NodeObj nodeobj = queue.poll();
+            int vertical = nodeobj.vertical;
+            Node node = nodeobj.node;
+            
+            if(!map.containsKey(vertical)){
+                map.put(vertical, node.data);
+            }
+            
+            if(node.left != null){
+                queue.add(new NodeObj(node.left, vertical - 1));
+            }
+            
+            if(node.right != null){
+                queue.add(new NodeObj(node.right, vertical + 1));
+            }
+        }
+        
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            result.add(entry.getValue());
+        }
+        
+        return result;
+    }
+}
