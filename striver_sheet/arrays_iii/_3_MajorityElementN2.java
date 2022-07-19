c2db9b2b326485fade49de905dbb254062708c32
Ayush Rao <ayushvijayrao@gmail.com>
2022-07-19 14:58:42 +0530

    Create _3_MajorityElementN2.java

---
+++ striver_sheet/arrays_iii/_3_MajorityElementN2.java
@@ -0,0 +1,22 @@
+package striver_sheet.arrays_iii;
+
+// https://leetcode.com/problems/majority-element/
+public class _3_MajorityElementN2 {
+    public int majorityElement(int[] nums) {
+        int curr = Integer.MIN_VALUE;
+        int count = 0;
+        for (int num : nums) {
+            if (curr == num) {
+                count++;
+            } else if (count == 0) {
+                curr = num;
+                count = 1;
+            } else {
+                count--;
+            }
+        }
+
+        // Majority is assured so we do not need to verify count
+        return curr;
+    }
+}
