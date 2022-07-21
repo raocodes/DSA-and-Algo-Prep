90d1f8b8ceacbb2515224bc3b3c866efd8f5ada0
Ayush Rao <ayushvijayrao@gmail.com>
2022-07-21 11:57:06 +0530

    Create _2_CopyListWithRandomPointer.java

---
+++ striver_sheet/linkedlistandarrays/_2_CopyListWithRandomPointer.java
@@ -0,0 +1,38 @@
+package striver_sheet.linkedlistandarrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// https://leetcode.com/problems/copy-list-with-random-pointer/
+public class _2_CopyListWithRandomPointer {
+    public Node copyRandomList(Node head) {
+        Map<Node, Node> mapping = new HashMap<>();
+
+        if (head == null) {
+            return null;
+        }
+
+        Node newhead = new Node(head.val);
+        Node temp = head;
+        Node tempnew = newhead;
+        mapping.put(head, newhead);
+        mapping.put(null, null);
+
+        while (temp.next != null) {
+            tempnew.next = new Node(temp.next.val);
+            mapping.put(temp.next, tempnew.next);
+            tempnew = tempnew.next;
+            temp = temp.next;
+        }
+
+        temp = head;
+        tempnew = newhead;
+        while (temp != null) {
+            tempnew.random = mapping.get(temp.random);
+            tempnew = tempnew.next;
+            temp = temp.next;
+        }
+
+        return newhead;
+    }
+}
