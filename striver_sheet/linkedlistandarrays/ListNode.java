e1d314b1ca84d17d473eac142066546559ef4569
Ayush Rao <ayushvijayrao@gmail.com>
2022-07-21 12:02:17 +0530

    Create ListNode.java

---
+++ striver_sheet/linkedlistandarrays/ListNode.java
@@ -0,0 +1,18 @@
+package striver_sheet.linkedlistandarrays;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
