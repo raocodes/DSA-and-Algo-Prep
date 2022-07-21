2745e9eb371374ed89294a93f1f6ff01b73bc6d9
Ayush Rao <ayushvijayrao@gmail.com>
2022-07-21 14:02:50 +0530

    Create SelectionSort.java

---
+++ sorting/SelectionSort.java
@@ -0,0 +1,28 @@
+package sorting;
+
+public class SelectionSort {
+    // O(n ^ 2)
+
+    public static void main(String[] args) {
+        int[] arr = new int[] { 6, 3, 9, 5, 2, 8 };
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            // arr.length - 1 because when there is one element left no need to sort
+            // The array is already sorted then
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
