28f0d767b8c25a9d9d4ea9ff8610436d4cae42cd
Ayush Rao <ayushvijayrao@gmail.com>
2022-07-22 11:23:05 +0530

    Create _6_MaxConsecutiveOnes.java

---
+++ striver_sheet/linkedlistandarrays/_6_MaxConsecutiveOnes.java
@@ -0,0 +1,21 @@
+package striver_sheet.linkedlistandarrays;
+
+// https://leetcode.com/problems/max-consecutive-ones/
+public class _6_MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int count = 0;
+
+        for (int element : nums) {
+            if (element == 1) {
+                count++;
+            } else {
+                max = Math.max(max, count);
+                count = 0;
+            }
+        }
+
+        max = Math.max(max, count);
+        return max;
+    }
+}
