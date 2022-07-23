315b4d572ecd3c8dd865f8b8989507e6759c51a9
Ayush Rao <ayushvijayrao@gmail.com>
2022-07-24 01:14:50 +0530

    Create _9_LargestSubStringWithoutRepeating.java

---
+++ slidingwindow_av/_9_LargestSubStringWithoutRepeating.java
@@ -0,0 +1,27 @@
+package slidingwindow_av;
+
+import java.util.HashMap;
+
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+public class _9_LargestSubStringWithoutRepeating {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int max = 0;
+        int curr = 0;
+        int left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            if (map.containsKey(s.charAt(right))) {
+                max = Integer.max(curr, max);
+                left = Math.max(map.get(s.charAt(right)) + 1, left);
+                curr = right - left + 1;
+                map.put(s.charAt(right), right);
+            } else {
+                curr++;
+                map.put(s.charAt(right), right);
+            }
+        }
+        max = Integer.max(curr, max);
+
+        return max;
+    }
+}
