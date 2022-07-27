8ce3a930ac02b3c932422cab499adb52aa7c38e8
Ayush Rao <ayushvijayrao@gmail.com>
2022-07-28 01:27:12 +0530

    Create TreeNode.java

---
+++ trees_striver/_32_CeilBST/TreeNode.java
@@ -0,0 +1,13 @@
+package trees_striver._32_CeilBST;
+
+public class TreeNode<T> {
+    T data;
+    TreeNode<T> left;
+    TreeNode<T> right;
+
+    TreeNode(T data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+};
