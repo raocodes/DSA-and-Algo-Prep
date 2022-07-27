ca13065d593557a860f0587e9c28251091d55b30
Ayush Rao <ayushvijayrao@gmail.com>
2022-07-28 01:27:18 +0530

    Create TreeNode.java

---
+++ trees_striver/_33_FloorBST/TreeNode.java
@@ -0,0 +1,13 @@
+package trees_striver._33_FloorBST;
+
+public class TreeNode<T> {
+    T data;
+    TreeNode<T> left;
+    TreeNode<T> right;
+
+    TreeNode(T data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+};
