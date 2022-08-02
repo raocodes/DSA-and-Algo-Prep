fed0eb77ff2ebdaf4a43f92f70c444e2d3305741
Ayush Rao <ayushvijayrao@gmail.com>
2022-08-02 17:30:41 +0530

    Create _10_TopKFrequent.java

---
+++ heaps/_10_TopKFrequent.java
@@ -0,0 +1,45 @@
+package heaps;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+// https://leetcode.com/problems/top-k-frequent-elements/
+public class _10_TopKFrequent {
+    public int[] topKFrequent(int[] nums, int k) {
+        Queue<QueueData> pq = new PriorityQueue<>((o1, o2) -> {
+            return o1.freq - o2.freq;
+        });
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            pq.offer(new QueueData(entry.getValue(), entry.getKey()));
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        int[] result = new int[pq.size()];
+        int count = 0;
+        while (!pq.isEmpty()) {
+            result[count++] = pq.poll().val;
+        }
+
+        return result;
+    }
+}
+
+class QueueData {
+    int freq;
+    int val;
+
+    QueueData(int freq, int val) {
+        this.freq = freq;
+        this.val = val;
+    }
+}
