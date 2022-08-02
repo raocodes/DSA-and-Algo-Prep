acbdd791b01da12d24a80c2f28383e17f26b8f2c
Ayush Rao <ayushvijayrao@gmail.com>
2022-08-02 20:09:44 +0530

    Update _10_TopKFrequent.java

--- heaps/_10_TopKFrequent.java
+++ heaps/_10_TopKFrequent.java
@@ -7,6 +7,16 @@ import java.util.Queue;
 
 // https://leetcode.com/problems/top-k-frequent-elements/
 public class _10_TopKFrequent {
+    class QueueData {
+        int freq;
+        int val;
+
+        QueueData(int freq, int val) {
+            this.freq = freq;
+            this.val = val;
+        }
+    }
+
     public int[] topKFrequent(int[] nums, int k) {
         Queue<QueueData> pq = new PriorityQueue<>((o1, o2) -> {
             return o1.freq - o2.freq;
@@ -31,15 +41,5 @@ public class _10_TopKFrequent {
         }
 
         return result;
-    }
-}
-
-class QueueData {
-    int freq;
-    int val;
-
-    QueueData(int freq, int val) {
-        this.freq = freq;
-        this.val = val;
     }
 }
