6390c5adcba150e4cef605b61ea7f5cd73e084ec
Ayush Rao <ayushvijayrao@gmail.com>
2022-08-08 11:50:44 +0530

    Create GcdLcm.java

---
+++ GcdLcm.java
@@ -0,0 +1,20 @@
+public class GcdLcm {
+    static long gcd(long big, long small) {
+        if (small == 0) {
+            return big;
+        }
+        return gcd(small, big % small);
+    }
+
+    static long lcm(long A, long B, long GCD) {
+        return (A * B) / GCD;
+    }
+
+    static Long[] lcmAndGcd(Long A, Long B) {
+        long big = A > B ? A : B;
+        long small = A < B ? A : B;
+        long gcdres = gcd(big, small);
+        long lcmres = lcm(big, small, gcdres);
+        return new Long[] { lcmres, gcdres };
+    }
+}
