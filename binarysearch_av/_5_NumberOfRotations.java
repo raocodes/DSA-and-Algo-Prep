39be9e2e22f5025fb83e73bdb127bf1c677a9acb
Ayush Rao <ayushvijayrao@gmail.com>
2022-08-10 00:52:40 +0530

    Update _5_NumberOfRotations.java

--- binarysearch_av/_5_NumberOfRotations.java
+++ binarysearch_av/_5_NumberOfRotations.java
@@ -3,18 +3,41 @@ package binarysearch_av;
 // https://practice.geeksforgeeks.org/problems/rotation4723/1/
 public class _5_NumberOfRotations {
     int findKRotation(int arr[], int n) {
-        int low = 0;
-        int high = n - 1;
+        // If the array was rotated in clockwise then we do return n - mid
 
-        while (low < high) {
-            int mid = low + (high - low) / 2;
+        int start = 0;
+        int end = n - 1;
 
-            if (arr[mid] > arr[high]) {
-                low = mid + 1;
-            } else {
-                high = mid;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int prev = (mid + n - 1) % n;
+            int next = (mid + 1) % n;
+
+            // System.out.println(start + " " + end + " " + mid);
+
+            if (arr[start] < arr[end]) {
+                // It is sorted
+                // So first will be the obvious answer
+                return start;
+            }
+
+            // If it is the smallest element
+            // It will have bigger elements on it's left and right
+            if (arr[mid] <= arr[next] && arr[mid] <= arr[prev]) {
+                return mid;
+            }
+
+            if (arr[start] <= arr[mid]) {
+                // If the beginning is lesser than or equal to mid
+                // The right is unsorted
+                start = mid + 1;
+            } else if (arr[end] >= arr[mid]) {
+                // If the end is greater than or equal to mid
+                // The left is unsorted
+                end = mid - 1;
             }
         }
-        return low;
+
+        return 0;
     }
 }
