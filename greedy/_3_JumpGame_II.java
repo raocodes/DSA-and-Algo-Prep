610fe362d8d82ffe8dd74c15a84ffbc300338711
Ayush Rao <ayushvijayrao@gmail.com>
2022-08-09 18:06:10 +0530

    Create _3_JumpGame_II.java

---
+++ greedy/_3_JumpGame_II.java
@@ -0,0 +1,25 @@
+package greedy;
+
+// https://leetcode.com/problems/jump-game-ii/
+public class _3_JumpGame_II {
+    public int jump(int[] nums) {
+        int result = 0;
+        int left = 0;
+        int right = 0;
+        
+        while(right < nums.length - 1){
+            int max = Integer.MIN_VALUE;
+            
+            for(int i = left; i <= right; i++){
+                max = Math.max(max, i + nums[i]);
+            }
+            
+
+            left = right + 1;
+            right = max;
+            result++;
+        }
+        
+        return result;
+    }
+}
