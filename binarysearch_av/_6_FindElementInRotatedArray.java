d59c0558219fd2ceee17280bcdc0752feec57ae9
Ayush Rao <ayushvijayrao@gmail.com>
2022-08-10 00:52:43 +0530

    Update _6_FindElementInRotatedArray.java

--- binarysearch_av/_6_FindElementInRotatedArray.java
+++ binarysearch_av/_6_FindElementInRotatedArray.java
@@ -1,58 +1,63 @@
 package binarysearch_av;
 
-// https://leetcode.com/problems/search-in-rotated-sorted-array/submissions/
-// https://leetcode.com/problems/search-in-rotated-sorted-array-ii/submissions/
-
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
 public class _6_FindElementInRotatedArray {
-    public int search(int[] nums, int target) {
+    private int findPivot(int[] nums) {
         int start = 0;
         int end = nums.length - 1;
+        int result = -1;
 
-        while (start < end) {
-            int mid = start + ((end - start) / 2);
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int prev = (mid + nums.length - 1) % nums.length;
+            int next = (mid + 1) % nums.length;
 
-            if (nums[mid] > nums[end]) {
+            if (nums[start] < nums[end]) {
+                result = start;
+                break;
+            }
+
+            if (nums[mid] <= nums[next] && nums[mid] <= nums[prev]) {
+                result = mid;
+                break;
+            }
+
+            if (nums[start] <= nums[mid]) {
                 start = mid + 1;
-            } else {
-                end = mid;
+            } else if (nums[end] >= nums[mid]) {
+                end = mid - 1;
             }
         }
 
-        if (target > nums[nums.length - 1]) {
-            end = start - 1;
-            start = 0;
-        } else {
-            end = nums.length - 1;
+        return result;
+    }
+
+    private int bSearch(int[] nums, int start, int end, int target) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
         }
 
-        while (start <= end) {
-            int mid = start + ((end - start) / 2);
-            if (nums[mid] == target) return mid;
-            else if (target < nums[mid]) end = mid - 1;
-            else start = mid + 1;
-        }
         return -1;
     }
 
-    public boolean searchii(int[] nums, int target) {
-        int start = 0, end = nums.length - 1;
-        while (start <= end) {
-            int mid = start + ((end - start) / 2);
-            if (nums[mid] == target) return true;
-            if (nums[mid] > nums[start]) {
-                // The left half is sorted
-                if (target < nums[mid] && target >= nums[start]) end = mid - 1;
-                else start = mid + 1;
-            } else if (nums[mid] < nums[start]) {
-                // The right half is sorted
-                if (target > nums[mid] && target <= nums[end]) start = mid + 1;
-                else end = mid - 1;
-            } else {
-                // Cannot figure out if the array is sorted so we just move start
-                // here start == mid and we already checked if it is target so we ignore value
-                start++;
-            }
+    public int search(int[] nums, int target) {
+        int pivot = findPivot(nums);
+        int left = bSearch(nums, 0, pivot - 1, target);
+        int right = bSearch(nums, pivot, nums.length - 1, target);
+
+        if (left == -1 && right == -1) {
+            return -1;
+        } else if (left == -1) {
+            return right;
+        } else {
+            return left;
         }
-        return false;
     }
 }
