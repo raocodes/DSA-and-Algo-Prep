85edefa8d72823fa4843d53e8c84e5f09d171a69
Ayush Rao <ayushvijayrao@gmail.com>
2022-08-10 15:42:54 +0530

    Update _8_CeilInSortedArray.java

--- binarysearch_av/_8_CeilInSortedArray.java
+++ binarysearch_av/_8_CeilInSortedArray.java
@@ -5,26 +5,20 @@ public class _8_CeilInSortedArray {
     public static int ceilingInSortedArray(int n, int x, int[] arr) {
         int start = 0;
         int end = n - 1;
-        int mid = -1;
+        int res = -1;
 
         while (start <= end) {
-            mid = start + ((end - start) / 2); // To prevent overflow
-            if (arr[mid] == x)
+            int mid = start + ((end - start) / 2); // To prevent overflow
+            if (arr[mid] == x) {
                 return arr[mid];
-            else if (x < arr[mid])
+            } else if (arr[mid] > x) {
                 end = mid - 1;
-            else
+                res = arr[mid];
+            } else {
                 start = mid + 1;
+            }
         }
 
-        if (arr[mid] > x) {
-            return arr[mid];
-        }
-
-        if (arr[mid] < x && mid == n - 1) {
-            return -1;
-        }
-
-        return arr[mid + 1];
+        return res;
     }
 }
