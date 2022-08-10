ceacf312ebfd0db822e0aecd73fc23c4e4a6bbef
Ayush Rao <ayushvijayrao@gmail.com>
2022-08-11 01:21:01 +0530

    Create _3_KokoEatingBananas.java

---
+++ binarysearch_neetcode/_3_KokoEatingBananas.java
@@ -0,0 +1,39 @@
+package binarysearch_neetcode;
+
+// https://leetcode.com/problems/koko-eating-bananas/
+public class _3_KokoEatingBananas {
+    private int calculateHours(int k, int[] piles) {
+        int hours = 0;
+        double rate = k;
+
+        for (int pile : piles) {
+            hours += Math.ceil(pile / rate);
+        }
+
+        return hours;
+    }
+
+    public int minEatingSpeed(int[] piles, int h) {
+        int max = Integer.MIN_VALUE;
+        for (int pile : piles) {
+            max = Math.max(pile, max);
+        }
+
+        int min = 1;
+        int result = max;
+
+        while (min <= max) {
+            int mid = min + (max - min) / 2;
+            int hours = calculateHours(mid, piles);
+
+            if (hours <= h) {
+                result = Math.min(result, mid);
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+
+        return result;
+    }
+}
