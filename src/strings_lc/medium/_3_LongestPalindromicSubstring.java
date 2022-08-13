995ff9c5c9209d7e681997ce8f208b2ed6504cdc
Ayush Rao <ayushvijayrao@gmail.com>
2022-08-14 01:44:43 +0530

    Create _3_LongestPalindromicSubstring.java

---
+++ src/strings_lc/medium/_3_LongestPalindromicSubstring.java
@@ -0,0 +1,41 @@
+package strings_lc.medium;
+
+// https://leetcode.com/problems/longest-palindromic-substring/
+public class _3_LongestPalindromicSubstring {
+    public String longestPalindrome(String s) {
+        int resultlen = 0;
+        String result = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            int left, right;
+
+            // Odd
+            left = i;
+            right = i;
+
+            while (left >= 0 && right <= s.length() - 1 && s.charAt(left) == s.charAt(right)) {
+                if (right - left + 1 > resultlen) {
+                    result = s.substring(left, right + 1);
+                    resultlen = right - left + 1;
+                }
+                left--;
+                right++;
+            }
+
+            // Even
+            left = i;
+            right = i + 1;
+
+            while (left >= 0 && right <= s.length() - 1 && s.charAt(left) == s.charAt(right)) {
+                if (right - left + 1 > resultlen) {
+                    result = s.substring(left, right + 1);
+                    resultlen = right - left + 1;
+                }
+                left--;
+                right++;
+            }
+        }
+
+        return result;
+    }
+}
