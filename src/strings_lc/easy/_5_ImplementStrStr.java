5c5a81c4d64e7359b4dfffd8dfcbbac7c92875e3
Ayush Rao <ayushvijayrao@gmail.com>
2022-08-14 01:44:35 +0530

    Create _5_ImplementStrStr.java

---
+++ src/strings_lc/easy/_5_ImplementStrStr.java
@@ -0,0 +1,19 @@
+package strings_lc.easy;
+
+// https://leetcode.com/problems/implement-strstr/
+public class _5_ImplementStrStr {
+    public int strStr(String haystack, String needle) {
+        int i = 0;
+        int len = needle.length();
+
+        while (i <= haystack.length() - needle.length()) {
+            // System.out.println(haystack.substring(i, i + len));
+            if (haystack.substring(i, i + len).equals(needle)) {
+                return i;
+            }
+            i++;
+        }
+
+        return -1;
+    }
+}
