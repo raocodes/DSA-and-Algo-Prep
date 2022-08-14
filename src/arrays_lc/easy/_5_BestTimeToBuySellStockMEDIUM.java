cdf73d158ec54eebecb6adfb5e3997d47a46cee9
raocodes <ayushvijayrao@gmail.com>
2022-08-14 17:51:40 +0530

    Update _5_BestTimeToBuySellStockMEDIUM.java

--- src/arrays_lc/easy/_5_BestTimeToBuySellStockMEDIUM.java
+++ src/arrays_lc/easy/_5_BestTimeToBuySellStockMEDIUM.java
@@ -43,4 +43,23 @@ public class _5_BestTimeToBuySellStockMEDIUM {
         // Sell = 0
         return find(0, 1, prices);
     }
+
+    public int maxProfitTabulation(int[] prices) {
+        int[][] dp = new int[prices.length + 1][2];
+
+        dp[prices.length][1] = 0;
+        dp[prices.length][0] = 0;
+
+        for (int i = prices.length - 1; i >= 0; i--) {
+            for (int j = 0; j < 2; j++) {
+                if (j == 1) {
+                    dp[i][j] = Math.max(dp[i + 1][0] - prices[i], dp[i + 1][1]);
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][1] + prices[i], dp[i + 1][0]);
+                }
+            }
+        }
+
+        return dp[0][1];
+    }
 }
