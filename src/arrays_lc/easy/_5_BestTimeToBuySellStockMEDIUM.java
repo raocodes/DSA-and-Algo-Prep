b4e9af10efb21bcae7228871b671828e8e1b512c
raocodes <ayushvijayrao@gmail.com>
2022-08-14 17:11:17 +0530

    Create _5_BestTimeToBuySellStockMEDIUM.java

---
+++ src/arrays_lc/easy/_5_BestTimeToBuySellStockMEDIUM.java
@@ -0,0 +1,46 @@
+package arrays_lc.easy;
+
+import java.util.Arrays;
+
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+public class _5_BestTimeToBuySellStockMEDIUM {
+    int[][] dp;
+
+    public int find(int index, int buy, int[] prices) {
+        if (index == prices.length) {
+            return 0;
+        }
+
+        if (dp[index][buy] != -1) {
+            return dp[index][buy];
+        }
+
+        if (buy == 1) {
+            // We have to purchase
+            return dp[index][buy] = Math.max(find(index + 1, 0, prices) - prices[index],
+                    find(index + 1, 1, prices));
+        } else {
+            // We have to sell
+            return dp[index][buy] = Math.max(find(index + 1, 1, prices) + prices[index],
+                    find(index + 1, 0, prices));
+        }
+    }
+
+    public int maxProfit(int[] prices) {
+        dp = new int[prices.length + 1][2];
+
+        dp[prices.length][1] = 0;
+        dp[prices.length][0] = 0;
+
+        for (int i = prices.length - 1; i >= 0; i--)
+
+
+            for (int[] row : dp) {
+                Arrays.fill(row, -1);
+            }
+
+        // Buy = 1
+        // Sell = 0
+        return find(0, 1, prices);
+    }
+}
