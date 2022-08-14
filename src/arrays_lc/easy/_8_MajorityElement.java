e80dd21c2b8f0d838fc700cc381a2c25ff46a492
raocodes <ayushvijayrao@gmail.com>
2022-08-14 17:11:24 +0530

    Create _8_MajorityElement.java

---
+++ src/arrays_lc/easy/_8_MajorityElement.java
@@ -0,0 +1,20 @@
+package arrays_lc.easy;
+
+// https://leetcode.com/problems/majority-element/
+public class _8_MajorityElement {
+    public int majorityElement(int[] nums) {
+        int curr = Integer.MIN_VALUE;
+        int count = 0;
+        for (int num : nums) {
+            if (curr == num) {
+                count++;
+            } else if (count == 0) {
+                curr = num;
+                count = 1;
+            } else {
+                count--;
+            }
+        }
+        return curr;
+    }
+}
