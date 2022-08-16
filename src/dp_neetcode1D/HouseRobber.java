d3a4e709ccbff06cd66fe96919dac203016831ef
raocodes <ayushvijayrao@gmail.com>
2022-08-16 18:42:04 +0530

    Added neetcode dp 1d solutions

---
+++ src/dp_neetcode1D/HouseRobber.java
@@ -0,0 +1,30 @@
+package dp_neetcode1D;
+
+import java.util.Arrays;
+
+// https://leetcode.com/problems/house-robber/
+public class HouseRobber {
+    int[] dp;
+
+    public int solve(int index, int[] nums) {
+        if (index >= nums.length) {
+            return 0;
+        }
+
+        if (dp[index] != -1) {
+            return dp[index];
+        }
+
+        // Rob the house
+        int rob = nums[index] + solve(index + 2, nums);
+        int skip = solve(index + 1, nums);
+
+        return dp[index] = Math.max(rob, skip);
+    }
+
+    public int rob(int[] nums) {
+        dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        return solve(0, nums);
+    }
+}
---
+++ src/dp_neetcode1D/WordBreak.java
@@ -0,0 +1,47 @@
+package dp_neetcode1D;
+
+import java.util.Arrays;
+import java.util.List;
+
+// https://leetcode.com/problems/word-break/
+public class WordBreak {
+    int[][] dp;
+
+    public boolean find(int left, int right, String s, List<String> wordDict) {
+        if (left > right) {
+            return false;
+        }
+
+        if (dp[left][right] != -1) {
+            return dp[left][right] == 0 ? false : true;
+        }
+
+        if (wordDict.contains(s.substring(left, right + 1))) {
+            return true;
+        }
+
+        for (int k = left; k < right; k++) {
+            boolean leftres = find(left, k, s, wordDict);
+            dp[left][k] = leftres == true ? 1 : 0;
+
+            boolean rightres = find(k + 1, right, s, wordDict);
+            dp[k + 1][right] = rightres == true ? 1 : 0;
+
+            if (rightres && leftres) {
+                return true;
+            }
+        }
+
+        dp[left][right] = 0;
+        return false;
+
+    }
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        dp = new int[s.length()][s.length()];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        return find(0, s.length() - 1, s, wordDict);
+    }
+}
