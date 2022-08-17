fff3fba82858c9830e2035fa717854d65f066e49
raocodes <ayushvijayrao@gmail.com>
2022-08-17 19:35:34 +0530

    Create _25_JumpGame.java

---
+++ src/arrays_lc/medium/_25_JumpGame.java
@@ -0,0 +1,15 @@
+package arrays_lc.medium;
+
+// https://leetcode.com/problems/jump-game/
+public class _25_JumpGame {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+
+        return goal == 0;
+    }
+}
