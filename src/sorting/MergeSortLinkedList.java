51140881fb8bf3d66179e50ec6cfbddea2b7e112
raocodes <ayushvijayrao@gmail.com>
2022-08-20 18:57:39 +0530

    Create MergeSortLinkedList.java

---
+++ src/sorting/MergeSortLinkedList.java
@@ -0,0 +1,75 @@
+package sorting;
+
+// https://leetcode.com/problems/sort-list/
+public class MergeSortLinkedList {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode left = head;
+        ListNode mid = findMid(head);
+        ListNode right = mid.next;
+        mid.next = null;
+
+        left = sortList(left);
+        right = sortList(right);
+
+        return merge(left, right);
+    }
+
+    public ListNode merge(ListNode left, ListNode right) {
+        ListNode temp = new ListNode();
+        ListNode ans = temp;
+
+        while (left != null && right != null) {
+            if (left.val <= right.val) {
+                temp.next = left;
+                left = left.next;
+            } else {
+                temp.next = right;
+                right = right.next;
+            }
+            temp = temp.next;
+        }
+
+        if (left != null) {
+            temp.next = left;
+        }
+
+        if (right != null) {
+            temp.next = right;
+        }
+
+        return ans.next;
+    }
+
+    public ListNode findMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
