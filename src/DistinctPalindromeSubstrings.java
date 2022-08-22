de5dcba999738da8f7936a87f3deac2d6a8f28bf
raocodes <ayushvijayrao@gmail.com>
2022-08-22 17:23:40 +0530

    Update DistinctPalindromeSubstrings.java

--- src/DistinctPalindromeSubstrings.java
+++ src/DistinctPalindromeSubstrings.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 
 // Similar to Longest palindromic substring from lc sheet
 // https://leetcode.com/problems/palindromic-substrings/
-public class DistincPalindromeSubstrings {
+public class DistinctPalindromeSubstrings {
     int palindromeSubStrs(String str) {
         HashSet<String> hash = new HashSet<>();
         for (int i = 0; i < str.length(); i++) {
