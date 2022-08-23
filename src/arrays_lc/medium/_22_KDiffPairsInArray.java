879539dbb0d2eb348cafc610936ea5a5e04b0286
raocodes <ayushvijayrao@gmail.com>
2022-08-23 23:38:48 +0530

    Update _22_KDiffPairsInArray.java

--- src/arrays_lc/medium/_22_KDiffPairsInArray.java
+++ src/arrays_lc/medium/_22_KDiffPairsInArray.java
@@ -1,25 +1,31 @@
 package arrays_lc.medium;
 
-import java.util.Arrays;
 import java.util.HashSet;
 
 // https://leetcode.com/problems/k-diff-pairs-in-an-array/
 public class _22_KDiffPairsInArray {
     public int findPairs(int[] nums, int k) {
-        Arrays.sort(nums);
         HashSet<Integer> hash = new HashSet<>();
-        int count = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (hash.contains(nums[i] - k)) {
-                count++;
-                int temp = nums[i];
-                while (i + 1 < nums.length && temp == nums[i + 1]) {
-                    i++;
-                }
+        int numrepeat = 0;
+        int prev = Integer.MIN_VALUE;
+        for (int num : nums) {
+            boolean temp = hash.add(num);
+            if (temp == false && num != prev) {
+                prev = num;
+                numrepeat++;
             }
+        }
 
-            hash.add(nums[i]);
+        if (k == 0) {
+            return numrepeat;
+        }
+
+        int count = 0;
+        for (int num : hash) {
+            if (hash.contains(num - k)) {
+                count++;
+            }
         }
 
         return count;
