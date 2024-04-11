662d6b4f1ccc770f87e76debc87224b38af63653
raocodes <ayushvijayrao@gmail.com>
2024-04-11 23:47:14 +0530

    Create _3_TwoSum.java

---
+++ src/neetcode_150/arrays_and_hashing/_3_TwoSum.java
@@ -0,0 +1,19 @@
+package neetcode_150.arrays_and_hashing;
+
+public class _3_TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> valueIndexMap = new HashMap<>();
+
+        int result = 0;
+        int index = 0;
+        for (; index < nums.length; index++) {
+            if (valueIndexMap.containsKey(target - nums[index])) {
+                result = valueIndexMap.get(target - nums[index]);
+                break;
+            }
+            valueIndexMap.put(nums[index], index);
+        }
+
+        return new int[] { index, result };
+    }
+}
